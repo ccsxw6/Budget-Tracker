129ca3bf4daab4c77469c59d019c9c622ba07273
ccsxw6 <ccsxw6@gmail.com>
2021-03-25 09:11:01 -0500

    changing server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -30,8 +30,6 @@ mongoose.connect(
   }
 );
 
-app.use(require("./routes/api.js"));
+require("./routes/api.js")(app)
 
-app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
-});
+app.listen(PORT, () => console.log(`App running on port ${PORT}!`));
