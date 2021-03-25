851b1b508bfaf28f129194093461f03819b5b2ba
ccsxw6 <ccsxw6@gmail.com>
2021-03-25 09:14:44 -0500

    still trying to change server.js

--- server.js
+++ server.js
@@ -3,22 +3,23 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = process.env.PORT || 3000;
-
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(logger("dev"));
 
 app.use(compression());
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static("public"));
 
 // mongoose.connect("mongodb://localhost/budget", {
 //   useNewUrlParser: true,
 //   useFindAndModify: false
 // });
+
+let MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
 
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost/budget",
@@ -33,3 +34,5 @@ mongoose.connect(
 require("./routes/api.js")(app)
 
 app.listen(PORT, () => console.log(`App running on port ${PORT}!`));
+
+// Your app is trying to connect to port 3000, which is a local port. I would go back over the guide making sure you give the port and database development/database options
