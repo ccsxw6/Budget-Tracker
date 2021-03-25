0a6eb5b1eb694bcc4228f0a21f2c7954324017f5
ccsxw6 <ccsxw6@gmail.com>
2021-03-25 08:44:58 -0500

    adding favicon

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,6 +4,7 @@ const FILES_TO_CACHE = [
     "/index.js", 
     "/styles.css", 
     "/indexedDB.js",
+    "/favicon.ico", 
     "/icons/icon-192x192.png", 
     "/icons/icon-512x512.png",
     "/manifest.webmanifest",
