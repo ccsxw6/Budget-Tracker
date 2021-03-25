ee65240d56406975a99adc7d3709ffb2fe252c9e
ccsxw6 <ccsxw6@gmail.com>
2021-03-24 19:28:34 -0500

    adding cache variables to service-worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,1 +1,13 @@
-console.log("Hi from your service-worker.js file!")
+const FILES_TO_CACHE = [
+    "/", 
+    "/index.html", 
+    "/index.js", 
+    "/favicon.ico", 
+    "/styles.css", 
+    "/icons/icon-144x144.png",
+    "/icons/icon-192x192.png", 
+    "/icons/icon-512x512.png"
+  ];
+
+  const CACHE_NAME = "static-cache-v2";
+  const DATA_CACHE_NAME = "data-cache-v1";
